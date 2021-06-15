8b0da4cf1e3bb5ae7bbbe0b6e7f805bbbd9e2835
Asaju Enitan Michael <mikkysam52@gmail.com>
2021-06-15 22:17:09 +0100

    UP

--- controllers/contact/index.js
+++ controllers/contact/index.js
@@ -1,18 +1,44 @@
 const ContactUs = require("../../models/ContactUs");
 
-const { tryCatchError } = require("../../utils/errorHandlers");
+const { tryCatchError, normalError } = require("../../utils/errorHandlers");
 const {
   successNoData,
+  successWithData,
 } = require("../../utils/successHandler");
 
 exports.createContact = async (req, res, next) => {
   try {
     await ContactUs.create(req.body);
-    return successNoData(
-      res,
-      200,
-      "Contact enquiry submitted succesfully",
-    );
+    return successNoData(res, 200, "Contact enquiry submitted successfully");
+  } catch (err) {
+    return tryCatchError(res, err);
+  }
+};
+
+exports.listAllContactRequests = async (req, res) => {
+  try {
+    let { page = 1, limit = 20 } = req.query;
+    const contacts = await ContactUs.find()
+      .sort("-createdAt")
+      .limit(limit * 1)
+      .skip((page - 1) * limit);
+    const count = await ContactUs.countDocuments();
+    const data = {
+      contacts,
+      total: Math.ceil(count / limit),
+      page: parseInt(page),
+    };
+    return successWithData(res, 200, "contact details fetched", contacts);
+  } catch (err) {
+    return tryCatchError(res, err);
+  }
+};
+
+exports.getOneContact = async (req, res) => {
+  try {
+    const contact = await ContactUs.findById(req.params.id);
+    if (!contact) return normalError(res, 404, "enquiry not found");
+    return successWithData(res, 200, "enquiry fetched", contact);
   } catch (err) {
     return tryCatchError(res, err);
   }
--- routes/contact/index.js
+++ routes/contact/index.js
@@ -4,5 +4,7 @@ const contactController = require("../../controllers/contact");
 const router = express.Router();
 
 router.post("/create", contactController.createContact);
+router.get("/", contactController.listAllContactRequests);
+router.get("/", contactController.getOneContact);
 
 module.exports = router;
