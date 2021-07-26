befdc68489cb7535759c16aed9ae454f60460890
Asaju Enitan Michael <asajuenitan@gmail.com>
2021-07-26 12:23:06 +0100

    🐞 FIX(user): reset password

--- controllers/auth/passwordReset.js
+++ controllers/auth/passwordReset.js
@@ -2,8 +2,8 @@ const PublicUser = require("../../models/PublicUser");
 const TokenModel = require("../../models/Token");
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
-const {sendSms} = require("../../utils/sendSms");
-const {generateOtp} = require('../../utils/otp');
+const { sendSms } = require("../../utils/sendSms");
+const { generateOtp } = require("../../utils/otp");
 
 const {
   normalError,
@@ -26,7 +26,7 @@ exports.requestPasswordReset = async (req, res) => {
       phoneNumber: req.body.phoneNumber,
     });
     if (!user) return normalError(res, 404, "account does not exist");
-    let token = await TokenModel.findOne({userID: user._id});
+    let token = await TokenModel.findOne({ userID: user._id });
     if (token) await token.deleteOne();
     let resetToken = crypto.randomBytes(32).toString("hex");
     const otp = generateOtp();
@@ -41,7 +41,7 @@ exports.requestPasswordReset = async (req, res) => {
       res,
       200,
       "otp sent to your phone number. please be patient, the message may take a few minutes",
-      {tokenID: newToken._id},
+      { tokenID: newToken._id },
     );
   } catch (err) {
     return tryCatchError(res, err);
@@ -50,7 +50,7 @@ exports.requestPasswordReset = async (req, res) => {
 
 exports.verifyOtp = async (req, res) => {
   try {
-    const token = await TokenModel.findOne({otp: req.body.otp});
+    const token = await TokenModel.findOne({ otp: req.body.otp });
     if (!token) return normalError(res, 400, "invalid otp code");
     await TokenModel.findByIdAndUpdate(token._id, {
       otpVerified: true,
@@ -67,7 +67,7 @@ exports.sendOtp = async (req, res) => {
       phoneNumber: req.body.phoneNumber,
     });
     if (!user) return normalError(res, 404, "user not found");
-    const token = await TokenModel.findOne({userID: user._id});
+    const token = await TokenModel.findOne({ userID: user._id });
     if (!token)
       return normalError(
         res,
@@ -77,15 +77,18 @@ exports.sendOtp = async (req, res) => {
     const otp = generateOtp();
     const newToken = await TokenModel.findByIdAndUpdate(
       token._id,
-      {otp},
-      {new: true},
+      { otp },
+      { new: true },
     );
-    await sendSms(user.phoneNumber, `Your OTP code is: ${newToken.otp}.`);
+    await sendSms(
+      user.phoneNumber,
+      `Your OTP code is: ${newToken.otp}.`,
+    );
     return successWithData(
       res,
       200,
       "otp sent to your phone number. please be patient, the message may take a few minutes",
-      {tokenID: newToken._id},
+      { tokenID: newToken._id },
     );
   } catch (err) {
     return tryCatchError(res, err);
@@ -113,8 +116,8 @@ exports.resendOtp = async (req, res) => {
     const otp = generateOtp();
     const newToken = await TokenModel.findByIdAndUpdate(
       token._id,
-      {otp, otpVerified: false},
-      {new: true},
+      { otp, otpVerified: false },
+      { new: true },
     );
     await sendSms(user.phoneNumber, newToken.otp);
     return successNoData(
@@ -136,7 +139,7 @@ exports.resendOtp = async (req, res) => {
 exports.resetPassword = async (req, res) => {
   try {
     let passwordResetToken = await TokenModel.findById(
-      req.body.TokenID,
+      req.body.tokenID,
     );
     if (!passwordResetToken)
       return normalError(
